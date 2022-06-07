8a2dc175fdda45bbe5c2552a554f3f6d429e289e
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-06-07 13:28:50 +0300

    initial commit

---
+++ LearningDiaryMae/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningDiaryMae
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Topic> diary = new List<Topic>();
+            bool stopLoop = false;
+
+            while (stopLoop == false)
+            {
+                Console.WriteLine("Do you want to add an entry? Yes/no");
+                string answer = Console.ReadLine();
+
+                if (answer.Equals("no", StringComparison.OrdinalIgnoreCase))
+                {
+                    stopLoop = true;
+                    break;
+                }
+
+                Topic newTopic = new Topic();
+
+                Console.WriteLine("What is the title of your study?");
+                newTopic.Title = Console.ReadLine();
+
+                Console.WriteLine("Describe the area of study: ");
+                newTopic.Description = Console.ReadLine();
+
+                Console.WriteLine("How much time do you need for studying the topic?");
+                newTopic.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Did you use a source? Yes/no");
+                answer = Console.ReadLine();
+
+                if (answer.Equals("yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Which source did you use?");
+                    newTopic.Source = Console.ReadLine();
+                }
+
+                Console.WriteLine("When did you start studying? YYYY/MM/DD");
+                newTopic.StartLearningDate = Convert.ToDateTime(Console.ReadLine());
+
+                Console.WriteLine("Is your study complete? Yes/no");
+                answer = Console.ReadLine();
+
+                if (answer.Equals("yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    newTopic.InProgress = false;
+                }
+
+                else
+                {
+                    newTopic.InProgress = true;
+                }
+
+                if (newTopic.InProgress == false)
+                {
+                    Console.WriteLine("When did you finish with the topic? YYYY/MM/DD");
+                    newTopic.CompletionDate = Convert.ToDateTime(Console.ReadLine());
+                }
+
+                diary.Add(newTopic);
+            }
+
+            for (int i=0; i < diary.Count; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ LearningDiaryMae/Topic.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningDiaryMae
+{
+    class Topic
+    {
+        public Topic()
+        {
+
+        }
+
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public double EstimatedTimeToMaster
+        {
+            get;
+            set;
+        }
+
+        public string Source
+        {
+            get;
+            set;
+        }
+
+        public DateTime StartLearningDate
+        {
+            get;
+            set;
+        }
+
+        public bool InProgress
+        {
+            get;
+            set;
+        }
+
+        public DateTime CompletionDate
+        {
+            get;
+            set;
+        }
+
+        public double TimeSpent()
+        {
+            return Convert.ToDouble(CompletionDate-StartLearningDate);
+        }
+
+        public string GetTitle()
+        {
+            return Title;
+        }
+    }
+}
