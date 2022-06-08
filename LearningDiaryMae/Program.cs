bda9a4f4dc5cb3a72dced2394b8a68fc1fecff53
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-06-08 13:41:08 +0300

    ended working on list

--- LearningDiaryMae/Program.cs
+++ LearningDiaryMae/Program.cs
@@ -10,7 +10,7 @@ namespace LearningDiaryMae
         {
             bool exit = false;
             int counter = 0;
-            List<Topic> diary = new List<Topic>();
+
 
             do
             {
@@ -21,85 +21,85 @@ namespace LearningDiaryMae
                                       "2. Print a current list of topics\n" +
                                       "3. Edit a topic\n" +
                                       "4. Exit the app");
-                int answer = Convert.ToInt32(Console.ReadLine());
+                    int answer = Convert.ToInt32(Console.ReadLine());
 
-                switch (answer)
-                {
-                    case 1:
-                        Topic newTopic = new Topic();
-                        newTopic.Id = counter;
-                        counter++;
+                    switch (answer)
+                    {
+                        case 1:
+                            Topic newTopic = new Topic();
+                            newTopic.Id = counter;
+                            counter++;
 
-                        Console.WriteLine("Title: ");
-                        newTopic.Title = Console.ReadLine();
+                            Console.WriteLine("Title: ");
+                            newTopic.Title = Console.ReadLine();
 
-                        Console.WriteLine("Describe the area of study: ");
-                        newTopic.Description = Console.ReadLine();
+                            Console.WriteLine("Describe the area of study: ");
+                            newTopic.Description = Console.ReadLine();
 
-                        Console.WriteLine("How much time do you need for studying the topic?");
-                        newTopic.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("How much time do you need for studying the topic?");
+                            newTopic.EstimatedTimeToMaster = Convert.ToDouble(Console.ReadLine());
 
-                        Console.WriteLine("Did you use a source? Yes/no");
-                        string input = Console.ReadLine();
+                            Console.WriteLine("Did you use a source? Yes/no");
+                            string input = Console.ReadLine();
 
-                        if (input.Equals("yes", StringComparison.OrdinalIgnoreCase))
-                        {
-                            Console.WriteLine("Which source did you use?");
-                            newTopic.Source = Console.ReadLine();
-                        }
+                            if (input.Equals("yes", StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine("Which source did you use?");
+                                newTopic.Source = Console.ReadLine();
+                            }
 
-                        Console.WriteLine("When did you start studying? YYYY/MM/DD");
-                        newTopic.StartLearningDate = Convert.ToDateTime(Console.ReadLine());
+                            Console.WriteLine("When did you start studying? YYYY/MM/DD");
+                            newTopic.StartLearningDate = Convert.ToDateTime(Console.ReadLine());
 
-                        Console.WriteLine("Is your study complete? Yes/no");
-                        input = Console.ReadLine();
+                            Console.WriteLine("Is your study complete? Yes/no");
+                            input = Console.ReadLine();
 
-                        if (input.Equals("yes", StringComparison.OrdinalIgnoreCase))
-                        {
-                            newTopic.InProgress = false;
-                        }
+                            if (input.Equals("yes", StringComparison.OrdinalIgnoreCase))
+                            {
+                                newTopic.InProgress = false;
+                            }
 
-                        else
-                        {
-                            newTopic.InProgress = true;
-                        }
+                            else
+                            {
+                                newTopic.InProgress = true;
+                            }
 
-                        if (newTopic.InProgress == false)
-                        {
-                            Console.WriteLine("When did you finish with the topic? YYYY/MM/DD");
-                            newTopic.CompletionDate = Convert.ToDateTime(Console.ReadLine());
-                        }
+                            if (newTopic.InProgress == false)
+                            {
+                                Console.WriteLine("When did you finish with the topic? YYYY/MM/DD");
+                                newTopic.CompletionDate = Convert.ToDateTime(Console.ReadLine());
+                            }
 
-                        diary.Add(newTopic);
-                        break;
+                            
+                            break;
 
-                    case 2:
-                        foreach (Topic item in diary)
-                        {
-                            Console.WriteLine("Title: " + item.GetTitle() + ". Id: " + item.Id);
-                        }
-                        break;
+                        case 2:
+                            foreach (Topic item in diary)
+                            {
+                                Console.WriteLine("Title: " + item.GetTitle() + ". Id: " + item.Id);
+                            }
 
-                    case 3:
+                            break;
 
-                        break;
+                        case 3:
+                            Console.WriteLine("Function to be added...");
+                            break;
 
-                    case 4:
-                        exit = true;
-                        break;
+                        case 4:
+                            Console.WriteLine("Exiting app.");
+                            exit = true;
+                            break;
 
-                    default:
-                        Console.WriteLine("Did you choose a number between 1 and 4?");
-                        break;
-                }
-
+                        default:
+                            Console.WriteLine("Did you choose a number between 1 and 4?");
+                            break;
+                    }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Did you choose a number between 1 and 4?");
                     continue;
                 }
-
             } while (exit == false);
         }
     }
